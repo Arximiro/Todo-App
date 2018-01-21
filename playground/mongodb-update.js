4b8c673ebbd2bf7ba569bef3ea39ceb40e0705e8
David Ridgley <Arximiro@outlook.com>
2018-01-20 23:03:38 -0500

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,33 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        console.log('Unable to connect to MongoDB server');
+    } else {
+        console.log('Connected to MongoDB server');
+        const myDb = db.db('TodoApp');
+
+        myDb.collection('Todos').findOneAndUpdate({ _id: ObjectID('5a63f9639855573868136575') }, {
+            $set: {
+                completed: true
+            }
+        }, {
+                returnOriginal: false
+            }).then((result) => {
+                console.log(result);
+            });
+
+
+        db.close();
+    }
+});
+
+// --- Notes ---
+// This files demonstrates how data in the db can be updated.
+// The first argument to findOneAndUpdate is the filter, next is the updates, next is the options(optional), then a callback(optional).
+// Here for options returnOriginal is passed in and set to false, so the updated item is returned. It is set to true by default.
+// If no callback function is passed in a promise is returned.
+
+// The entry is selected by _id, then the using the $set operator, the completed property is set to true.
+// You can also use the $inc operator, choosing a field and incrementing it. For example $inc: {age: 1} would increment age by 1.
+// $inc can also be used to decrement, for example $inc: {age: -1} would decrement age by 1.
