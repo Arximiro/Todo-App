cb33569e7a44187d06efd0d4ce47633c4ec86682
David Ridgley <Arximiro@outlook.com>
2018-01-21 15:33:45 -0500

    Added GET /todos route

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const {User} = require('./models/user');
 
 const app = express();
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
     const todo = new Todo({
@@ -16,6 +16,14 @@ app.post('/todos', (req, res) => {
 
     todo.save().then((doc) => {
         res.send(doc);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos})
     }).catch((e) => {
         res.status(400).send(e);
     });
@@ -30,7 +38,13 @@ module.exports = {app};
 // --- Notes ---
 // bodyParser.json returns a function to express.
 // app.use registers bodyParser as middleware for express to use.
+// body-parser parses incoming request bodies in a middleware before your handlers, available under the req.body property.
+
 // app.post does an HTTP POST request to the url.
 // A new todo is created, setting the text property to whatever came back from the request.body.text.
 // Then that todo is saved, then the returned doc is sent back to the user.
 // If the save fails then it returns status code 400 which is bad request.
+
+// app.get does an HTTP GET request to the url.
+// Todo.find() returns all data from the collection.
+// In the returned promise then call, that data is sent to the user.
