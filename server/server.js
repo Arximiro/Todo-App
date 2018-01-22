93e8d10be855a607647e4156202cc4f76fd892f1
David Ridgley <Arximiro@outlook.com>
2018-01-21 20:08:49 -0500

    Added GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
+const { ObjectID } = require('mongodb');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const {mongoose} = require('./db/mongoose');
-const {Todo} = require('./models/todo');
-const {User} = require('./models/user');
+const { mongoose } = require('./db/mongoose');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
 
 const app = express();
 
@@ -23,17 +24,34 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
-        res.send({todos})
+        res.send({todos});
     }).catch((e) => {
         res.status(400).send(e);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send();        
+    } else {
+        Todo.findById(id).then((todo) => {
+            if (!todo) {
+                res.status(404).send();
+            }
+            res.send({todo});
+        }).catch((e) => {
+            res.status(400).send();
+        });
+    }
 });
 
 app.listen(3000, () => {
     console.log('Started on port 3000.');
 });
 
-module.exports = {app};
+module.exports = { app };
 
 // --- Notes ---
 // bodyParser.json returns a function to express.
