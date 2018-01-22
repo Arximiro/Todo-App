68dca662a1770b6fb45121f7dfb3fa3bac6e2e69
David Ridgley <Arximiro@outlook.com>
2018-01-21 19:34:32 -0500

    Add queries script

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,36 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('../server/db/mongoose');
+const {Todo} = require('../server/models/todo');
+
+const id = '5a652378d881ea0348ec38b5';
+
+if (!ObjectID.isValid(id)) {
+    console.log('Invalid ID');
+} else {
+    Todo.find({
+        _id: id
+    }).then((todos) => {
+        console.log('----------------------------------------');
+        console.log(`Todos: ${todos}`);
+        console.log('----------------------------------------');
+    });
+    
+    Todo.findOne({
+        _id: id
+    }).then((todo) => {
+        console.log(`Todo: ${todo}`);
+        console.log('----------------------------------------');
+    });
+    
+    Todo.findById(id).then((todo) => {
+        console.log(`Find By ID: ${todo}`);
+        console.log('----------------------------------------');
+    }).catch((e) => console.log('wut' + e));
+}
+
+// ObjectID method .isValid() is used to check that the structure of an id is the required format for an ObjectID.
+// Mongoose casts _id to ObjectID in the backgrouns so we don't have to explicitly make that cast in our code.
+// The find() method returns all items in the collection matching the search criteria.
+// The findOne() method only finds and returns the first result matching the search criteria.
+// The findById() method searches for an item in the collection with an _id matching the one passed in. It's basically findOne just simpler.
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -77,13 +77,14 @@ describe('GET /todos', () => {
 // After that the expect calls are made, expecting a status code of 200 which is OK.
 // Since an object with the response is returned from the post, the next expect call takes that object and checks to make sure the body matches the text varaible.
 // After that .end is called, which takes two args, an error and the response. If there was an error in the above calls, then done is called ending the testing.
-// If there was no error the testing continues, calling find() to search all entries in the db and using the returned promise to take what was found and make assertions.
+// If there was no error the testing continues, next up calling find({text}) to search all entries in the collection
+// for an entry with the text matching the text variable, then using the returned promise to take what was found and make assertions.
 // First epxecting the length of the returned object toBe 1, and expecting the text property of the 1st obj in the returned array to match the text variable.
 // After that done() is called ending the testing, and lastly if there was any errors those are caught, then done is called passing in that error.
 
 // In the 2nd POST todos test, invalid data is used in the form of an empty object.
 // The first assertion is that a status code 400 should be returned which is Bad Request.
-// Then at the end the last assertion is that the length of the todos collection is 0.
+// Then at the end the last assertion is that the length of the todos collection is 3.
 
 
 // --- GET Todos Notes ---
