bf593adb410d5c91e79669f0a97d75ced1498d66
David Ridgley <Arximiro@outlook.com>
2018-01-30 05:55:48 -0500

    Updated config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,17 @@
-const env = process.env.NODE_ENV || 'development';
+let env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if (env === 'development' || env === 'test') {
+    const config = require('./config.json');
+    const envConfig = config[env];
+
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key]
+    });
 }
+
+// When setting a variable to a property of another object, you must use the [bracket] notation.
+// envConfig pulls off an array from the config JSON object based on what the current env is and what it matches up to in config.json.
+// Then it's looped over with forEach resulting in
+// process.env[PORT] = env.config[VALUE]
+// process.env[MONGODB_URI] = env.config[VALUE]
+// Etc... for each environment variable.
